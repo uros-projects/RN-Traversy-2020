df6895fd20fe8a9d8ab4719f3100e1cd637e2ae9
Uros <uros.tomasevic.work@gmail.com>
2025-06-21 13:09:59 +0200

    Add components and functions for AddItem, DeleteItem

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { Alert, FlatList, StyleSheet, View } from 'react-native';
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
+import AddItem from '../components/AddItem';
 import Header from '../components/Header';
 import ListItem from '../components/ListItem';
 
@@ -13,13 +14,29 @@ const App = () => {
 		{ id: uuidv4(), name: 'Juice' },
 	]);
 
+	const deleteItem = (id) => {
+		setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+	};
+
+	const addItem = (text) => {
+		if (!text) {
+			Alert.alert('Error', 'Please enter an item', [{ text: 'OK' }]);
+		} else {
+			setItems((prevItems) => {
+				return [{ id: uuidv4(), name: text }, ...prevItems];
+			});
+		}
+	};
+
 	return (
 		<View style={styles.container}>
 			<Header title='Shopping List' />
+			<AddItem addItem={addItem} />
 			<FlatList
 				data={items}
-				renderItem={({ item }) => <ListItem item={item} />}
-				// keyExtractor={}
+				renderItem={({ item }) => (
+					<ListItem item={item} deleteItem={deleteItem} />
+				)}
 			/>
 		</View>
 	);
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const ListItem = ({ item }) => {
+const ListItem = ({ item, deleteItem }) => {
 	return (
 		<TouchableOpacity style={styles.listItem}>
 			<View style={styles.listItemView}>
 				<Text style={styles.listItemText}>{item.name}</Text>
+				<Icon
+					name='times'
+					size={18}
+					color='firebrick'
+					onPress={() => deleteItem(item.id)}
+				/>
 			</View>
 		</TouchableOpacity>
 	);
